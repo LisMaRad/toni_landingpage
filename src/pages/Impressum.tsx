450b2fb9f4f36a84ab8e8293451a042573e09afa
Lisa <fhs45899@fh-salzburg.ac.at>
2024-06-23 21:19:26 +0200

    Change color of impressum heading

--- src/pages/Impressum.tsx
+++ src/pages/Impressum.tsx
@@ -7,7 +7,7 @@ const Impressum: React.FC = () => {
     return (
         <div className="absolute top-[60px] flex flex-col justify-between h-[calc(100%-60px)]">
             <div className="p-6 md:px-20 md:py-16">
-                <h1>Impressum</h1>
+                <h1 className="dark:text-primary-color-dm">Impressum</h1>
                 <p>Informationspflicht laut § 5 E-Commerce Gesetz, Offenlegungspflicht laut §25 Mediengesetz.</p>
                 <p>Tel.: <a href="tel:+4915175059609">+49 151 75 05 96 09</a><br/>
                     E-Mail: <a href="mailto:toni.navigation@gmail.com">toni.navigation@gmail.com</a></p>
