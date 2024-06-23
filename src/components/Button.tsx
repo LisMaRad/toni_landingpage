68d6472ffb529e0f68d993ed067e008e1d1de3f8
Lisa <fhs45899@fh-salzburg.ac.at>
2024-06-23 17:00:21 +0200

    Add icon to buttons, adjust icon size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
 export function Button({children, classes, onClick}: ButtonProps) {
     return (
         <button
-            className={`flex justify-center items-center bg-primary-color-lm px-5 py-2 rounded-button h-fit dark:bg-primary-color-dm hover:bg-orange-accent ${classes}`}
+            className={`flex justify-center items-center gap-1 sm:gap-2 md:gap-4 bg-primary-color-lm px-3 py-1 md:px-5 md:py-2 rounded-button h-fit dark:bg-primary-color-dm hover:bg-orange-accent ${classes}`}
             onClick={onClick}>{children}</button>
     );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,18 +2,20 @@ import React, {useContext, useEffect, useState} from 'react';
 import {Button} from "./Button";
 import {Link, useNavigate} from "react-router-dom";
 import {DarkModeContext} from "../contexts/DarkModeContext";
+import {VscColorMode} from "react-icons/vsc";
 
 export function Sidebar() {
     const navigate = useNavigate();
 
-    const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+    const {darkMode, toggleDarkMode} = useContext(DarkModeContext);
 
     return (
         <div
-            className="w-full p-3 bg-primary-color-dm dark:bg-primary-color-lm flex flex-row items-center gap-4 z-10 md:items-start md:flex-col justify-between md:h-[100vh] md:w-[20%] fixed top-0 md:p-6">
+            className="w-full p-3 bg-primary-color-dm dark:bg-primary-color-lm flex flex-row items-center gap-4 z-10 md:items-start md:flex-col justify-between md:h-[100vh] md:w-[20%] fixed top-0 md:p-4 lg:p-6">
             <Link to="/"><img src={darkMode ? "./images/logo-light.svg" : "./images/logo-dark.svg"} alt="Toni Logo"
                               className="w-[72px] md:w-3/4 md:mx-auto md:mb-6"/></Link>
-            <div className="flex-1"><Button onClick={() => toggleDarkMode()} classes="ml-auto md:ml-0">Ansicht</Button></div>
+            <div className="flex-1"><Button onClick={() => toggleDarkMode()} classes="ml-auto md:ml-0"><VscColorMode className="text-primary-color-dm dark:text-primary-color-lm"/>Ansicht</Button>
+            </div>
 
             <Button onClick={() => navigate("/impressum")}>Impressum</Button>
         </div>
