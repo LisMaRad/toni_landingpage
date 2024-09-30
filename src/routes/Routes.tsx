74ab4ae447be5cd31d91798af8d3f32c7fb2ccbf
Philine Hoernemann <phhoernemann@web.de>
2024-09-30 14:57:47 +0200

    add routes and pages for app content

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,7 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Index from "../pages/Index";
 import Impressum from "../pages/Impressum";
+import AgbsApp from "../pages/AgbsApp";
+import ImpressumApp from "../pages/ImpressumApp";
+import {createBrowserRouter} from "react-router-dom";
+import NutzungsbedingungenApp from "../pages/NutzungsbedingungenApp";
+import DatenschutzApp from "../pages/DatenschutzApp";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +14,10 @@ export const router = createBrowserRouter([
         children: [
             { path: "", element: <Index /> },
             { path: "impressum", element: <Impressum /> },
+            { path: "app-impressum", element: <ImpressumApp /> },
+            { path: "app-agbs", element: <AgbsApp /> },
+            { path: "app-datenschutz", element: <DatenschutzApp /> },
+            { path: "app-nutzungsbedingungen", element: <NutzungsbedingungenApp /> },
         ],
     },
 ]);
