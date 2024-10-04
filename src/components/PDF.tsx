528264949e6595c4da3645c75ea069d25faa59dc
Philine Hoernemann <phhoernemann@web.de>
2024-10-04 10:02:13 +0200

    change Widerrufsformular.pdf

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -1,23 +1,10 @@
-import React, { useCallback } from 'react';
-import jsPDF from "jspdf";
-import {Button} from "./Button";
+import React from 'react';
 
 const PDF = () => {
-    const generatePDF = useCallback(() => {
-        const doc = new jsPDF();
-        doc.text(" Widerrufsformular ", 20, 20);
-        doc.text(" Hiermit widerrufe ich den von mir abgeschlossenen Vertrag \nüber die Erbringung " +
-            "der folgenden Dienstleistung: ", 20, 40);
-        doc.text(" Bestellt am: ", 20, 60);
-        doc.text(" Name: ", 20, 80);
-        doc.text(" Anschrift: ", 20, 100);
-        doc.text(" Datum, Unterschrift (nur bei Mitteilung auf Papier) ", 20, 120);
-        doc.save("widerrufsformular.pdf");
-    }, []);
 
     return (
         <div>
-            <Button onClick={generatePDF}>Vorlage herunterladen</Button>
+            <a className={"underline text-orange-accent"} href="widerrufsformular.pdf" download>Vorlage Widerrufsformular</a>
         </div>
     );
 }
